813cbaef66d48f1400a73804e6eef66654a2933f
albhasan <albhasan@gmail.com>
2014-08-19 16:19:37 +0200

    Stuff

--- shimc.lua
+++ shimc.lua
@@ -1,11 +1,38 @@
 #!/usr/bin/lua
 require("shimclient")
 
+function file_exists(file)
+	local f = io.open(file, "rb")
+	if f then f:close() end
+	return f ~= nil
+end
 
-local sdburl = "http://www.myserver.org:00000"
-local sdburls = "https://www.myserver.org:00000"
-local user = "myuser"
-local password = "mypassword"
+function lines_from(file)
+	if not file_exists(file) then return {} end
+	lines = {}
+	for line in io.lines(file) do 
+		lines[#lines + 1] = line
+	end
+	return lines
+end
+
+
+local file = 'conf.exe'
+local lines = lines_from(file)
+local sdburl = lines[1]
+local sdburls = lines[2]
+local user = lines[3]
+local password = lines[4]
+
+
+
+print(sdburl)
+print(sdburls)
+print(user)
+print(password)
+
+
+
 local auth = ""
 local sid = ""
 local query = "list('functions')"
@@ -46,3 +73,19 @@ print("\n", "Releasing session...")
 print(releasesession(sdburls, sid,auth))
 print("\n", "Loggin out...")
 print(logout(sdburls, auth))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
